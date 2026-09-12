540ea8ffbfddaad02f4d622f40b1d32cfdad87df
ankit04developer <4ankitbhattarai4@gmail.com>
2026-09-12 23:02:21 +0545

    Update day2_metacharacters.py

--- Python/Regex-Learning/examples/day2_metacharacters.py
+++ Python/Regex-Learning/examples/day2_metacharacters.py
@@ -1,1 +1,34 @@
+#. (Dot)
+#Inside the regular expression, a dot represents any character except
+#the newline character, which is \n
+
+import re
+data = "hello\nworld"
+pattern = r''
+matches = re.finditer(pattern, data)
+for match in matches:
+    print(match)
+    
+    
+
+# Metacharacter '+'
+#means preceding expression or character should repeat one or more times
+#with as many repetitions as possible.
+
+import re
+text = "My email addresses are shantanu@codeyug.com and shantanu10@gmail.com"
+pattern =  r'\w+@\w+\.\w+'
+email_adderesses = re.findall(pattern, text)
+print(email_adderesses)
+
+
+
+# Metacharacter '*'
+# a* matches zero or more 'a' characters, including zero occurrences.
+
+import re
+data = "hello Ankit"
+pattern = r'l*'
+matches = re.findall(pattern, data)
+print(matches)
 
