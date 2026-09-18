4ba0878461f9ebba89369884b6a425c12d4fa387
ankit04developer <4ankitbhattarai4@gmail.com>
2026-09-18 22:19:44 +0545

    Update day2_metacharacters.py

--- Python/Regex-Learning/examples/day2_metacharacters.py
+++ Python/Regex-Learning/examples/day2_metacharacters.py
@@ -34,3 +34,37 @@ print(matches)
 
 
 
+# Metacharacter '^'
+# It ensure that the pattern only matches when it appears at the beginning of the text.
+
+import re
+data = "4hello Ankit"
+pattern = r'^\d'
+matches = re.findall(pattern, data)
+if matches:
+    print("Starting with digits.")
+else:
+    print("Not starting with digits.")
+
+# negation of character classes
+
+# In this example: [^0-9] matches any single character that is not a digit.
+import re
+data = "hello 10 11world"
+pattern = r"[^0-9]"
+matches = re.findall(pattern, data)
+print(matches)
+
+
+# Metacharacter '$'
+# used to match the end of a line or the end of data.
+# It is just opposite of '^'
+
+import re
+data = '''hello Ankit333
+i am anish123'''
+pattern = r'\d{3}$'
+matches = re.finditer(pattern, data, re.MULTILINE)
+for match in matches:
+    print(match)
+
